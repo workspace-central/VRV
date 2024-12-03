0b68f8cc62e55cb7e3d74a8be18a9606353b2c83
Vmihira <vinjamurimihira@gmail.com>
2024-12-03 19:40:43 +0530

    updated map component

--- Frontend/src/components/Map.tsx
+++ Frontend/src/components/Map.tsx
@@ -30,7 +30,7 @@ export default function GlobalPresence() {
             <img
               src="Map.png"
               alt="World Map"
-              className="object-contain w-full h-full opacity-80"
+              className="object-contain w-full h-full opacity-55"
             />
           </div>
 
@@ -40,13 +40,13 @@ export default function GlobalPresence() {
               {/* Top Right Stat */}
               <div className="absolute top-0 right-0 max-w-[20vw]">
                 <div className="flex items-baseline gap-1 mb-1">
-                  <span className="absolute right-full border-t-4 border-dotted border-white w-8 md:w-32" />
+                  
                   <span className="font-mono text-3xl md:text-4xl lg:text-5xl">
                     12
                   </span>
                   <span className="text-xl md:text-2xl lg:text-3xl">+</span>
                 </div>
-                <p className="text-xs md:text-sm leading-tight text-gray-300">
+                <p className="text-xs md:text-sm leading-tight text-white">
                   Countries across five continents, with a strong Global Reach,
                   offering tailored cybersecurity solutions to ensure top tier
                   protection.
@@ -54,54 +54,54 @@ export default function GlobalPresence() {
               </div>
 
               {/* Left Side Stat */}
-              <div className="absolute top-1/4 left-0 max-w-[20vw] ml-20">
-                <div className="pt-14 font-mono text-3xl md:text-4xl lg:text-5xl tracking-wider">
+              <div className="absolute left-0 max-w-[20vw] ml-20">
+                <div className="pt-0 font-mono text-3xl md:text-4xl lg:text-5xl tracking-wider">
                   200
                 </div>
-                <p className="text-xs md:text-sm leading-tight text-gray-300">
+                <p className="text-xs md:text-sm leading-tight text-white">
                   International clients, including{" "}
                   <span className="italic">Fortune 500</span> companies and{" "}
                   <span className="italic">government agencies</span>.
                 </p>
-                <div className="absolute top-1/2 -right-8 md:-right-16 border-t-4 border-dotted border-white w-8 md:w-56" />
+                
               </div>
 
               {/* Bottom Left Stat */}
-              <div className="absolute bottom-60 left-0 max-w-[20vw] ml-36">
+              <div className="mt-44 pl-0 bottom-60 left-0 max-w-[20vw] ml-20">
                 <div className="font-mono text-2xl md:text-3xl lg:text-4xl tracking-wider mb-1">
                   1,000,000
                 </div>
-                <p className="text-xs md:text-sm leading-tight text-gray-300">
+                <p className="text-xs md:text-sm leading-tight text-white">
                   Our solution provides real-time threat detection and{" "}
                   <span className="italic">robust</span> protection for your
                   devices worldwide, securing{" "}
                   <span className="italic">endpoints</span> globally over
                 </p>
-                <div className="absolute top-1/2 -right-8 md:-right-16 border-t-4 border-dotted border-white w-8 md:w-20" />
+                
               </div>
 
               {/* Bottom Center Stat */}
-              <div className="absolute  bottom-0 left-2/3 -translate-x-1/2 max-w-[30vw] text-center">
-                <p className="text-xs md:text-sm mb-1 text-gray-300">
+              <div className="absolute bottom-0 left-2/3 -translate-x-1/2 max-w-[30vw] text-center">
+                <p className="text-xs md:text-sm mb-1 text-white">
                   Provides Proactive Monitoring with rate
                 </p>
-                <div className="font-mono text-3xl md:text-4xl lg:text-5xl tracking-wider mb-1">
+                <div className="font-mono text-3xl md:text-4xl lg:text-5xl tracking-wider mb-0">
                   500,000+
                 </div>
-                <p className="text-xs md:text-sm leading-tight text-gray-300">
+                <p className="text-xs md:text-sm leading-tight text-white">
                   hours of annual threat surveillance, ensuring real-time,
                   consistent defense against cyber risks for optimal security.
                 </p>
-                <div className="absolute bottom-full right-full border-t-4 border-r-4 border-dotted border-white w-8 md:w-16 h-4 md:h-8 left-40" />
+                
               </div>
 
               {/* Right Side Stat */}
-              <div className="absolute bottom-1/3 right-0 max-w-[20vw] text-right">
-                <p className="text-xs md:text-sm -mb-28 text-gray-300 ">
+              <div className="absolute  right-0 max-w-[20vw] text-right">
+                <p className="text-xs md:text-sm -mb-28 text-white ">
                   Recognized as a trusted leader in cybersecurity innovation and
                   excellence, we have a valuation reaching
                 </p>
-                <div className="flex items-baseline gap-1 justify-end">
+                <div className="flex items-baseline gap-1 justify-end ">
                   <span className="text-xl md:text-2xl lg:text-3xl">$</span>
                   <span className="font-mono text-3xl pt-0 md:text-4xl lg:text-5xl tracking-wider">
                     400
